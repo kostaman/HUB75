67dc7da04ac54bb06d01dd4326f240bccaeffac0
Peter Majchrak <petoknm@gmail.com>
2017-05-11 22:14:05 +0200

    Add Pixel8bit for smaller memory footprint
    Signed-off-by: Peter Majchrak <petoknm@gmail.com>

--- src/pixel.h
+++ src/pixel.h
@@ -2,13 +2,12 @@
 #define HUB75_PIXEL_H
 
 #include <cstdint>
-#include <bitset>
 
 namespace HUB75 {
 
     /**
-     * 5bit color depth per channel
-     * 0x0BBBBBGGGGGRRRRR
+     * 5 bit color depth per channel
+     * 0b0BBBBBGGGGGRRRRR
      */
     class Pixel16bit {
 
@@ -54,6 +53,59 @@ namespace HUB75 {
         void b(uint8_t v) {
             value &= ~(mask << 10);
             value |= (v & mask) << 10;
+        }
+
+    };
+
+
+    /**
+     * 2 bit color depth per channel
+     * 0b00BBGGRR
+     */
+    class Pixel8bit {
+
+        uint8_t value;
+
+        static const uint8_t mask = 0x03;
+
+    public:
+
+        static const size_t FrameCount = 2;
+
+        Pixel8bit() : value(0) {
+        }
+
+        Pixel8bit(uint8_t red, uint8_t green, uint8_t blue) : value(0) {
+            r(red);
+            g(green);
+            b(blue);
+        }
+
+        uint8_t r() {
+            return value & mask;
+        }
+
+        uint8_t g() {
+            return (value >> 2) & mask;
+        }
+
+        uint8_t b() {
+            return (value >> 4) & mask;
+        }
+
+        void r(uint8_t v) {
+            value &= ~mask;
+            value |= v & mask;
+        }
+
+        void g(uint8_t v) {
+            value &= ~(mask << 2);
+            value |= (v & mask) << 2;
+        }
+
+        void b(uint8_t v) {
+            value &= ~(mask << 4);
+            value |= (v & mask) << 4;
         }
 
     };
